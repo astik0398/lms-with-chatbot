310034addc4fa1515c59d8586dd33c65a8426c49
Kumar Astik <kumarastik0398@gmail.com>
2024-06-06 20:52:12 +0530

    added cors in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const {connection} = require('./db')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 const {userRouter} = require('./routes/user.route')
