d89453d0dab1e3c16a35ef383288a907ab63d0d0
Kumar Astik <kumarastik0398@gmail.com>
2024-06-10 18:15:58 +0530

    fixed chatbot infinite rendering

--- frontend/src/Pages/Chatbot.jsx
+++ frontend/src/Pages/Chatbot.jsx
@@ -4,57 +4,32 @@ import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 
 const Chatbot = () => {
-  const [userInput, setUserInput] = useState('');
-  const [response, setResponse] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
 
-  console.log(userInput);
-  useEffect(() => {
-    if (userInput) {
-      setIsLoading(true); // Set loading state to true when making the request
-      axios.post('https://lms-with-chatbot.onrender.com/chat', { userInput: userInput })
-        .then((res) => {
-          // Set the response received from the server
-          console.log(res);
-          setResponse(res.data.response);
-        })
-        .catch((error) => {
-          console.error('Error fetching response:', error);
-          // Handle error appropriately
-        })
-        .finally(() => {
-          setIsLoading(false); // Reset loading state after request completes
-        });
-    }
-  }, [userInput])
+  // const [userInput, setUserInput] = useState('');
+  // const [response, setResponse] = useState('');
+  // const [isLoading, setIsLoading] = useState(false);
 
-  const steps = [
-    {
-      id: '0',
-      message: 'Hey Geek!',
-      trigger: '1',
-    },
-    {
-      id: '1',
-      message: 'Please write your username',
-      trigger: '2',
-    },
-    {
-      id: '2',
-      user: true,
-      trigger: '3',
-    },
-    {
-      id: '3',
-      message: () => (isLoading ? 'Wait...' : response),
-      trigger: '3',
-    },
-  ];
+  // useEffect(() => {
+  //   if (userInput) {
+  //     setIsLoading(true)
+  //     axios.post('https://lms-with-chatbot.onrender.com/chat', { userInput: userInput })
+  //       .then((res) => {
+  //         console.log(res);
+  //         setResponse(res.data.response);
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error fetching response:', error);
+  //       })
+  //       .finally(() => {
+  //         setIsLoading(false)
+  //       });
+  //   }
+  // }, [userInput])
 
   const theme = {
-    background: '#C9FF8F',
+    background: 'rgb(201, 255, 143)',
     headerBgColor: '#197B22',
-    headerFontSize: '20px',
+    headerFontSize: '18px',
     botBubbleColor: '#0F3789',
     headerFontColor: 'white',
     botFontColor: 'white',
@@ -62,16 +37,54 @@ const Chatbot = () => {
     userFontColor: 'white',
   };
 
-  // Set some properties of the bot
-  const config = {
-    botAvatar: 'https://cdn-icons-png.flaticon.com/512/8649/8649595.png',
-    floating: true,
-  };
-
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <ChatBot headerTitle="GeekBot" steps={steps} {...config} />
+      <ChatBot
+      
+  steps={[
+    {
+      id: '1',
+      message: 'Hey there! Im Nichole, here to assist you with everything at Edu Junction. To get started, please enter your name. Thanks!',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      validator: (value) => {
+        if (!value) {
+          return 'Please Enter Your Name';
+        }
+        return true;
+      },
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Hi {previousValue}, nice to meet you! What can I help you with ?',
+      trigger: '4',
+    },
+    {
+      id: '4',
+      options: [
+        { value: 1, label: 'Courses', trigger: '5' },
+        { value: 2, label: 'About Edu Junction', trigger: '6' },
+      ],
+    },
+    {
+      id: '5',
+        message: 'Currently we are offering upto 5 courses but many courses are comming up very soon.',
+    },
+    {
+      id: '6',
+        message: 'Edu Junction is a learning management system where user can enroll in variety of courses and level up their skills.',
+        end: true,
+    }
+  ]}
+  floating = {true}
+  botAvatar = 'https://cdn-icons-png.flaticon.com/512/8649/8649595.png'
+  headerTitle='Edu_Junction Support'
+/>
       </ThemeProvider>
     </div>
   );
